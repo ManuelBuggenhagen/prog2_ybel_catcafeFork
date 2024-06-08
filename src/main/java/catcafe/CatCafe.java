e56fe04728cdecb8d5571d31f77e3d93817d4551
ManuelBuggenhagen <manuel.buggenhagen@fh-bielefeld.de>
2024-06-08 19:22:19 +0200

    (A07.3) test3.2

--- src/main/java/catcafe/CatCafe.java
+++ src/main/java/catcafe/CatCafe.java
@@ -3,6 +3,7 @@ package catcafe;
 import static java.util.Objects.requireNonNull;
 
 import java.util.Optional;
+
 import tree.Empty;
 import tree.Tree;
 import tree.TreeVisitor;
@@ -10,42 +11,63 @@ import tree.TreeVisitor;
 public class CatCafe {
     private Tree<FelineOverLord> clowder = new Empty<>();
 
-    /** adds a cat to the cat clowder */
+    /**
+     * adds a cat to the cat clowder
+     */
     public void addCat(FelineOverLord cat) {
         clowder = clowder.addData(requireNonNull(cat));
     }
 
-    /** returns amount of cat that currently exists */
+    /**
+     * returns amount of cat that currently exists
+     */
     public long getCatCount() {
         return clowder.size();
     }
 
-    public FelineOverLord getCatByName(String name) {
-        if (name == null) return null;
+    public Optional<FelineOverLord> getCatByName(String name) {
+        if (name == null) throw new NullPointerException("invalid parameter!");
 
         for (FelineOverLord c : clowder) {
-            if (c.name().equals(name)) return c;
+            if (c.name().equals(name)) {
+                return Optional.of(c);
+            }
         }
+        return Optional.empty();
 
-        return null;
+
+        /*
+        FelineOverLord d = clowder.stream()
+            .filter(c -> c.name().equals(name))
+            .findAny().orElseThrow();
+
+        return Optional.of(d);
+
+         */
     }
 
-    /** returns a cat that has a certain weight the weight is specified in the parameter */
-    public FelineOverLord getCatByWeight(int minWeight, int maxWeight) {
+
+    /**
+     * returns a cat that has a certain weight the weight is specified in the parameter
+     */
+    public Optional<FelineOverLord> getCatByWeight(int minWeight, int maxWeight) {
         if (minWeight < 0) throw new RuntimeException("cat with negative weight dont exist");
         if (maxWeight < minWeight) throw new RuntimeException("that doesnt make sense!");
 
         for (FelineOverLord c : clowder) {
-            if (c.weight() >= minWeight && c.weight() < maxWeight) return c;
+            if (c.weight() >= minWeight && c.weight() < maxWeight) {
+                return Optional.of(c);
+            }
         }
 
-        return null;
+        return Optional.empty();
     }
 
-    /** accept method for visitor pattern */
+
+    /**
+     * accept method for visitor pattern
+     */
     String accept(TreeVisitor<FelineOverLord> visitor) {
         return clowder.accept(visitor);
     }
-
-
 }
--- src/main/java/catcafe/Main.java
+++ src/main/java/catcafe/Main.java
@@ -1,5 +1,7 @@
 package catcafe;
 
+import java.util.NoSuchElementException;
+
 /**
  * Starter for the cat-café task.
  */
@@ -14,18 +16,29 @@ public class Main {
 
         cafe.addCat(new FelineOverLord("Miss Chief Sooky", 2));
         cafe.addCat(new FelineOverLord("Gwenapurr Esmeralda", 3));
-        cafe.addCat(new FelineOverLord("Morticia", 3));
+        cafe.addCat(new FelineOverLord("Morticia", 4));
         cafe.addCat(new FelineOverLord("Fitzby Darnsworth", 5));
 
-        System.out.println("Es schnurren " + cafe.getCatCount() + " Samtpfoetchen.");
+        System.out.println("\n\nEs schnurren " + cafe.getCatCount() + " Samtpfoetchen.");
 
-        FelineOverLord meow = cafe.getCatByWeight(3, 4);
-        System.out.println("Gewicht [3,4]: " + meow);
+        try {
+            FelineOverLord meow = cafe.getCatByWeight(3, 4).orElseThrow();
+            System.out.println("Gewicht [3,4]: " + meow);
 
-        meow = cafe.getCatByName("Morticia");
-        System.out.println("Name 'Morticia': " + meow);
+            meow = cafe.getCatByName("Morticia").orElseThrow();
+            System.out.println("Name 'Morticia': " + meow);
 
-        meow = cafe.getCatByName("Miss Chief Sooky");
-        System.out.println("Name 'Miss Chief Sooky': " + meow);
+            meow = cafe.getCatByName("Miss Chief Sooky").orElseThrow();
+            System.out.println("Name 'Miss Chief Sooky': " + meow);
+
+        } catch (NoSuchElementException e) {
+            System.out.println("noSuchElement: " + e.getMessage());
+        } catch (NullPointerException d) {
+            System.out.println("nullPunter: " + d.getMessage());
+        } catch (Exception x) {
+            System.out.println("exception: " + x.getMessage());
+        }
+
+        System.out.println("\n\n");
     }
 }
